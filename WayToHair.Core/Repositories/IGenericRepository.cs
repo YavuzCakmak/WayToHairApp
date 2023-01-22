7267d36e1c1a12fb03e976f8a39a8e755a6e1550
Yavuz Çakmak <yavuz.cakmak@affintybox.com>
2023-01-22 17:09:02 +0200

    Repository Implementation

---
+++ WayToHair.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace WayToHair.Core.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(long id);
+        IQueryable<T> GetAll(Expression<Func<T, bool>> expression);
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+        Task AddAsync(T entity);
+        Task AddRange(IEnumerable<T> entities);
+        void Update(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+    }
+}
