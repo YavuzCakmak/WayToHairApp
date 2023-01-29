6ea47ac8e79f3feb86ed4d129a3a25606b0dc0d4
Yavuz Çakmak <yavuz.cakmak@affintybox.com>
2023-01-29 23:30:15 +0200

    AppDbContext -- EntityConfigurations

---
+++ WayToHair.Repository/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WayToHair.Core;
+
+namespace WayToHair.Repository
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<ContactEntity> ContactEntity { get; set; }
+        public DbSet<FaqEntity> FaqEntity { get; set; }
+        public DbSet<SidebarEntity> SidebarEntity { get; set; }
+    }
+}
